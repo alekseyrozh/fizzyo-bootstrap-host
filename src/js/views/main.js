83c49e415f5058d55e8ac04c0327f6c215b0cbdb
Marcin Praski <martinp2p@gmail.com>
2017-12-03 17:19:02 +0000

    dunno

--- src/js/views/main.js
+++ src/js/views/main.js
@@ -1,533 +1,104 @@
-$(function(){
-  'use strict';
 
-  //convert Hex to RGBA
-  function convertHex(hex,opacity){
-    hex = hex.replace('#','');
-    var r = parseInt(hex.substring(0,2), 16);
-    var g = parseInt(hex.substring(2,4), 16);
-    var b = parseInt(hex.substring(4,6), 16);
 
-    var result = 'rgba('+r+','+g+','+b+','+opacity/100+')';
-    return result;
-  }
+    var ctx = document.getElementById("schedule").getContext('2d');
 
-  //Cards with Charts
-  var labels = ['January','February','March','April','May','June','July'];
-  var data = {
-    labels: labels,
-    datasets: [
-      {
-        label: 'My First dataset',
-        backgroundColor: $.brandPrimary,
-        borderColor: 'rgba(255,255,255,.55)',
-        data: [65, 59, 84, 84, 51, 55, 40]
-      },
-    ]
-  };
-  var options = {
-    maintainAspectRatio: false,
-    legend: {
-      display: false
-    },
-    scales: {
-      xAxes: [{
-        gridLines: {
-          color: 'transparent',
-          zeroLineColor: 'transparent'
+    var sess1 = 30;
+    var sess2 = 30;
+    var sess3 = 30;
+    var day = 60 * 24;
+
+    var gap = (day - sess1 - sess2 - sess3);
+
+    var data = {
+        datasets: [{
+            data: [gap / 3, sess1, gap / 3, sess2, gap / 3, sess3],
+            backgroundColor: [
+                'lightblue',
+                'lightgreen',
+                'lightblue',
+                'lightgreen',
+                'lightblue',
+                'lightgreen',
+                'lightblue'
+            ]
+        }],
+
+        // These labels appear in the legend and in the tooltips when hovering different arcs
+        // labels: [
+        //   'Session 1: 10:00am 00:30min',
+        //   'Yellow',
+        //   'Blue'
+        // ]
+    };
+
+    var options = {
+        legend: {
+            display: false
         },
-        ticks: {
-          fontSize: 2,
-          fontColor: 'transparent',
+        tooltips: {
+            enabled: false
         }
-
-      }],
-      yAxes: [{
-        display: false,
-        ticks: {
-          display: false,
-          min: Math.min.apply(Math, data.datasets[0].data) - 5,
-          max: Math.max.apply(Math, data.datasets[0].data) + 5,
-        }
-      }],
-    },
-    elements: {
-      line: {
-        borderWidth: 1
-      },
-      point: {
-        radius: 4,
-        hitRadius: 10,
-        hoverRadius: 4,
-      },
     }
-  };
-  var ctx = $('#card-chart1');
-  var cardChart1 = new Chart(ctx, {
-    type: 'line',
-    data: data,
-    options: options
-  });
 
-  var data = {
-    labels: labels,
-    datasets: [
-      {
-        label: 'My First dataset',
-        backgroundColor: $.brandInfo,
-        borderColor: 'rgba(255,255,255,.55)',
-        data: [1, 18, 9, 17, 34, 22, 11]
-      },
-    ]
-  };
-  var options = {
-    maintainAspectRatio: false,
-    legend: {
-      display: false
-    },
-    scales: {
-      xAxes: [{
-        gridLines: {
-          color: 'transparent',
-          zeroLineColor: 'transparent'
-        },
-        ticks: {
-          fontSize: 2,
-          fontColor: 'transparent',
-        }
-
-      }],
-      yAxes: [{
-        display: false,
-        ticks: {
-          display: false,
-          min: Math.min.apply(Math, data.datasets[0].data) - 5,
-          max: Math.max.apply(Math, data.datasets[0].data) + 5,
-        }
-      }],
-    },
-    elements: {
-      line: {
-        tension: 0.00001,
-        borderWidth: 1
-      },
-      point: {
-        radius: 4,
-        hitRadius: 10,
-        hoverRadius: 4,
-      },
-    }
-  };
-  var ctx = $('#card-chart2');
-  var cardChart2 = new Chart(ctx, {
-    type: 'line',
-    data: data,
-    options: options
-  });
-
-  var options = {
-    maintainAspectRatio: false,
-    legend: {
-      display: false
-    },
-    scales: {
-      xAxes: [{
-        display: false
-      }],
-      yAxes: [{
-        display: false
-      }],
-    },
-    elements: {
-      line: {
-        borderWidth: 2
-      },
-      point: {
-        radius: 0,
-        hitRadius: 10,
-        hoverRadius: 4,
-      },
-    }
-  };
-  var data = {
-    labels: labels,
-    datasets: [
-      {
-        label: 'My First dataset',
-        backgroundColor: 'rgba(255,255,255,.2)',
-        borderColor: 'rgba(255,255,255,.55)',
-        data: [78, 81, 80, 45, 34, 12, 40]
-      },
-    ]
-  };
-  var ctx = $('#card-chart3');
-  var cardChart3 = new Chart(ctx, {
-    type: 'line',
-    data: data,
-    options: options
-  });
-
-  //Random Numbers
-  function random(min,max) {
-    return Math.floor(Math.random()*(max-min+1)+min);
-  }
-
-  var elements = 16;
-  var labels = [];
-  var data = [];
-
-  for (var i = 2000; i <= 2000 + elements; i++) {
-    labels.push(i);
-    data.push(random(40,100));
-  }
-
-  var options = {
-    maintainAspectRatio: false,
-    legend: {
-      display: false
-    },
-    scales: {
-      xAxes: [{
-        display: false,
-        barPercentage: 0.6,
-      }],
-      yAxes: [{
-        display: false,
-      }]
-    },
-
-  };
-  var data = {
-    labels: labels,
-    datasets: [
-      {
-        backgroundColor: 'rgba(255,255,255,.3)',
-        borderColor: 'transparent',
-        data: data
-      },
-    ]
-  };
-  var ctx = $('#card-chart4');
-  var cardChart4 = new Chart(ctx, {
-    type: 'bar',
-    data: data,
-    options: options
-  });
-
-  //Main Chart
-  var elements = 27;
-  var data1 = [];
-  var data2 = [];
-  var data3 = [];
-
-  for (var i = 0; i <= elements; i++) {
-    data1.push(random(50,200));
-    data2.push(random(80,100));
-    data3.push(65);
-  }
-
-  var data = {
-    labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S', 'M', 'T', 'W', 'T', 'F', 'S', 'S', 'M', 'T', 'W', 'T', 'F', 'S', 'S', 'M', 'T', 'W', 'T', 'F', 'S', 'S'],
-    datasets: [
-      {
-        label: 'My First dataset',
-        backgroundColor: convertHex($.brandInfo,10),
-        borderColor: $.brandInfo,
-        pointHoverBackgroundColor: '#fff',
-        borderWidth: 2,
-        data: data1
-      },
-      {
-        label: 'My Second dataset',
-        backgroundColor: 'transparent',
-        borderColor: $.brandSuccess,
-        pointHoverBackgroundColor: '#fff',
-        borderWidth: 2,
-        data: data2
-      },
-      {
-        label: 'My Third dataset',
-        backgroundColor: 'transparent',
-        borderColor: $.brandDanger,
-        pointHoverBackgroundColor: '#fff',
-        borderWidth: 1,
-        borderDash: [8, 5],
-        data: data3
-      }
-    ]
-  };
-
-  var options = {
-    maintainAspectRatio: false,
-    legend: {
-      display: false
-    },
-    scales: {
-      xAxes: [{
-        gridLines: {
-          drawOnChartArea: false,
-        }
-      }],
-      yAxes: [{
-        ticks: {
-          beginAtZero: true,
-          maxTicksLimit: 5,
-          stepSize: Math.ceil(250 / 5),
-          max: 250
-        }
-      }]
-    },
-    elements: {
-      point: {
-        radius: 0,
-        hitRadius: 10,
-        hoverRadius: 4,
-        hoverBorderWidth: 3,
-      }
-    },
-  };
-  var ctx = $('#main-chart');
-  var mainChart = new Chart(ctx, {
-    type: 'line',
-    data: data,
-    options: options
-  });
-
-
-  //Social Box Charts
-  var labels = ['January','February','March','April','May','June','July'];
-
-  var options = {
-    responsive: true,
-    maintainAspectRatio: false,
-    legend: {
-      display: false,
-    },
-    scales: {
-      xAxes: [{
-        display:false,
-      }],
-      yAxes: [{
-        display:false,
-      }]
-    },
-    elements: {
-      point: {
-        radius: 0,
-        hitRadius: 10,
-        hoverRadius: 4,
-        hoverBorderWidth: 3,
-      }
-    }
-  };
-
-  var data1 = {
-    labels: labels,
-    datasets: [{
-      backgroundColor: 'rgba(255,255,255,.1)',
-      borderColor: 'rgba(255,255,255,.55)',
-      pointHoverBackgroundColor: '#fff',
-      borderWidth: 2,
-      data: [65, 59, 84, 84, 51, 55, 40]
-    }]
-  };
-  var ctx = $('#social-box-chart-1');
-  var socialBoxChart1 = new Chart(ctx, {
-    type: 'line',
-    data: data1,
-    options: options
-  });
-
-  var data2 = {
-    labels: labels,
-    datasets: [
-      {
-        backgroundColor: 'rgba(255,255,255,.1)',
-        borderColor: 'rgba(255,255,255,.55)',
-        pointHoverBackgroundColor: '#fff',
-        borderWidth: 2,
-        data: [1, 13, 9, 17, 34, 41, 38]
-      }
-    ]
-  };
-  var ctx = $('#social-box-chart-2').get(0).getContext('2d');
-  var socialBoxChart2 = new Chart(ctx, {
-    type: 'line',
-    data: data2,
-    options: options
-  });
-
-  var data3 = {
-    labels: labels,
-    datasets: [
-      {
-        backgroundColor: 'rgba(255,255,255,.1)',
-        borderColor: 'rgba(255,255,255,.55)',
-        pointHoverBackgroundColor: '#fff',
-        borderWidth: 2,
-        data: [78, 81, 80, 45, 34, 12, 40]
-      }
-    ]
-  };
-  var ctx = $('#social-box-chart-3').get(0).getContext('2d');
-  var socialBoxChart3 = new Chart(ctx, {
-    type: 'line',
-    data: data3,
-    options: options
-  });
-
-  var data4 = {
-    labels: labels,
-    datasets: [
-      {
-        backgroundColor: 'rgba(255,255,255,.1)',
-        borderColor: 'rgba(255,255,255,.55)',
-        pointHoverBackgroundColor: '#fff',
-        borderWidth: 2,
-        data: [35, 23, 56, 22, 97, 23, 64]
-      }
-    ]
-  };
-  var ctx = $('#social-box-chart-4').get(0).getContext('2d');
-  var socialBoxChart4 = new Chart(ctx, {
-    type: 'line',
-    data: data4,
-    options: options
-  });
+    var myDoughnutChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: data,
+        options: options
+    });
 
 
 
-  //Sparkline Charts
-  var labels = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
+    var input = function(visible) {
+        var inputDisplay = visible ? 'inline' : 'none';
+        var out = !visible ? 'inline' : 'none';
+        document.getElementById('in0').style.display = inputDisplay;
+        document.getElementById('in1').style.display = inputDisplay;
+        document.getElementById('in2').style.display = inputDisplay;
+        document.getElementById('in3').style.display = inputDisplay;
+        document.getElementById('in4').style.display = inputDisplay;
+        document.getElementById('in5').style.display = inputDisplay;
+        document.getElementById('in6').style.display = inputDisplay;
+        document.getElementById('in7').style.display = inputDisplay;
 
-  var options = {
-    legend: {
-      display: false,
-    },
-    scales: {
-      xAxes: [{
-        display:false,
-      }],
-      yAxes: [{
-        display:false,
-      }]
-    },
-    elements: {
-      point: {
-        radius: 0,
-        hitRadius: 10,
-        hoverRadius: 4,
-        hoverBorderWidth: 3,
-      }
-    },
-  };
+        document.getElementById('out0').style.display = out;
+        document.getElementById('out1').style.display = out;
+        document.getElementById('out2').style.display = out;
+        document.getElementById('out3').style.display = out;
+        document.getElementById('out4').style.display = out;
+        document.getElementById('out5').style.display = out;
+        document.getElementById('out6').style.display = out;
+        document.getElementById('out7').style.display = out;
+    };
 
-  var data1 = {
-    labels: labels,
-    datasets: [
-      {
-        backgroundColor: 'transparent',
-        borderColor: $.brandPrimary,
-        borderWidth: 2,
-        data: [35, 23, 56, 22, 97, 23, 64]
-      }
-    ]
-  };
-  var ctx = $('#sparkline-chart-1');
-  var sparklineChart1 = new Chart(ctx, {
-    type: 'line',
-    data: data1,
-    options: options
-  });
 
-  var data2 = {
-    labels: labels,
-    datasets: [
-      {
-        backgroundColor: 'transparent',
-        borderColor: $.brandDanger,
-        borderWidth: 2,
-        data: [78, 81, 80, 45, 34, 12, 40]
-      }
-    ]
-  };
-  var ctx = $('#sparkline-chart-2');
-  var sparklineChart2 = new Chart(ctx, {
-    type: 'line',
-    data: data2,
-    options: options
-  });
 
-  var data3 = {
-    labels: labels,
-    datasets: [
-      {
-        backgroundColor: 'transparent',
-        borderColor: $.brandWarning,
-        borderWidth: 2,
-        data: [35, 23, 56, 22, 97, 23, 64]
-      }
-    ]
-  };
-  var ctx = $('#sparkline-chart-3');
-  var sparklineChart3 = new Chart(ctx, {
-    type: 'line',
-    data: data3,
-    options: options
-  });
+    $('#modify-prescription').on('click', function(e) {
+        document.getElementById('modify-prescription').style.display = 'none';
 
-  var data4 = {
-    labels: labels,
-    datasets: [
-      {
-        backgroundColor: 'transparent',
-        borderColor: $.brandSuccess,
-        borderWidth: 2,
-        data: [78, 81, 80, 45, 34, 12, 40]
-      }
-    ]
-  };
-  var ctx = $('#sparkline-chart-4');
-  var sparklineChart4 = new Chart(ctx, {
-    type: 'line',
-    data: data4,
-    options: options
-  });
+        document.getElementById('modify-prescription-save').style.display = 'inline';
+        document.getElementById('modify-prescription-cancel').style.display = 'inline';
 
-  var data5 = {
-    labels: labels,
-    datasets: [
-      {
-        backgroundColor: 'transparent',
-        borderColor: '#d1d4d7',
-        borderWidth: 2,
-        data: [35, 23, 56, 22, 97, 23, 64]
-      }
-    ]
-  };
-  var ctx = $('#sparkline-chart-5');
-  var sparklineChart5 = new Chart(ctx, {
-    type: 'line',
-    data: data5,
-    options: options
-  });
+        input(true);
+    });
 
-  var data6 = {
-    labels: labels,
-    datasets: [
-      {
-        backgroundColor: 'transparent',
-        borderColor: $.brandInfo,
-        borderWidth: 2,
-        data: [78, 81, 80, 45, 34, 12, 40]
-      }
-    ]
-  };
-  var ctx = $('#sparkline-chart-6');
-  var sparklineChart6= new Chart(ctx, {
-    type: 'line',
-    data: data6,
-    options: options
-  });
 
-});
+    //
+    $('#modify-prescription-save').on('click', function(e) {
+        document.getElementById('modify-prescription').style.display = 'inline';
+
+        document.getElementById('modify-prescription-save').style.display = 'none';
+        document.getElementById('modify-prescription-cancel').style.display = 'none';
+
+
+        input(false);
+    });
+
+    $('#modify-prescription-cancel').on('click', function(e) {
+        document.getElementById('modify-prescription').style.display = 'inline';
+
+        document.getElementById('modify-prescription-save').style.display = 'none';
+        document.getElementById('modify-prescription-cancel').style.display = 'none';
+
+
+        input(false);
+    });
